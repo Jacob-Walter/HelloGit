1898d5f3ce9f6fdfd2e30d66561919e84b31563c
jonnyskelton <jonnyskelton@jonathans-mbp>
2022-08-26 18:09:52 -0500

    Added Semi Colons lol

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -10,8 +10,8 @@ public class Hello {
 		
 		System.out.println("My name is Jacob");
 		System.out.println("My major is Computer Science");
-		System.out.println("My name is Jonny")
-		System.out.println("My major is computer science")
+		System.out.println("My name is Jonny");
+		System.out.println("My major is computer science");
 	}
 
 }
